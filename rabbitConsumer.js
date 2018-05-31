5f4392b64bfc184067d32474a5d0ad86c1e79624
Jorge Ortega Traverso <j.ortega.traverso@accenture.com>
2018-05-31 20:40:52 +0200

    Viendo propiedades del mensaje recibido

--- rabbitConsumer.js
+++ rabbitConsumer.js
@@ -10,6 +10,14 @@ amqp.connect(url, function(err, conn) {
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
     ch.consume(q, function(msg) {
       console.log(" [x] Received %s", msg.content.toString());
+      console.log(msg.content['OrganizationId'])
+		console.log(msg.content['OrganizationId']);
+		console.log(msg.content['ActionId']);
+		console.log(msg.content['SessionId']);
+		console.log(msg.content['EnterpriseUrl']);
+		console.log(msg.content['PartnerUrl']);
+		console.log(msg.content['Notification']);
+
     }, {noAck: true});
   });
 });
