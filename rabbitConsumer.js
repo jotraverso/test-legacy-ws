14cf095d5c87fbfdc54dea1061d90a2d61f1d79c
Jorge Ortega Traverso <j.ortega.traverso@accenture.com>
2018-05-31 20:43:35 +0200

    Viendo propiedades del mensaje recibido

--- rabbitConsumer.js
+++ rabbitConsumer.js
@@ -3,21 +3,22 @@
 var amqp = require('amqplib/callback_api');
 var url = process.env.CLOUDAMQP_URL || "amqp://localhost";
 amqp.connect(url, function(err, conn) {
-  conn.createChannel(function(err, ch) {
-    var q = 'casePhaseNotificationQueue';
+    conn.createChannel(function(err, ch) {
+        var q = 'casePhaseNotificationQueue';
 
-    // ch.assertQueue(q, {durable: true});
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-    ch.consume(q, function(msg) {
-      console.log(" [x] Received %s", msg.content.toString());
-      console.log(msg.content['OrganizationId'])
-		console.log(msg.content['OrganizationId']);
-		console.log(msg.content['ActionId']);
-		console.log(msg.content['SessionId']);
-		console.log(msg.content['EnterpriseUrl']);
-		console.log(msg.content['PartnerUrl']);
-		console.log(msg.content['Notification']);
+        // ch.assertQueue(q, {durable: true});
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+        ch.consume(q, function(msg) {
+            console.log(" [x] Received %s", msg.content.toString());
+            var notif = JSON.parse(msg.content);
+            console.log(notif['OrganizationId'])
+            console.log(notif['OrganizationId']);
+            console.log(notif['ActionId']);
+            console.log(notif['SessionId']);
+            console.log(notif['EnterpriseUrl']);
+            console.log(notif['PartnerUrl']);
+            console.log(notif['Notification']);
 
-    }, {noAck: true});
-  });
+        }, { noAck: true });
+    });
 });
